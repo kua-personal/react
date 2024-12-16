8ed68a09131b86eba3626df705bc7b0a83e19b59
HoYoung <kgerust@gmail.com>
2024-12-17 01:33:15 +0900

    state 상태관리

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,26 +1,26 @@
 import './App.css'
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer.jsx";
-import Button from "./components/Button.jsx";
+import {useState} from "react";
 
 function App() {
-    const buttonProps = {
-        text: "메일",
-        color: "red",
-        a: 1,
-        b: 2
-    }
+    const [count, setCount] = useState(0)
+    const [light, setLight] = useState("OFF")
+
     return (
         <>
-            {/*<Header/>*/}
-            {/*<Main/>*/}
-            {/*<Footer/>*/}
-            <Button {...buttonProps}/>
-            <Button text={"카페"}/>
-            <Button text={"블로그"}>
-                <div>자식 요소</div>
-            </Button>
+            <div>
+                <h1>{light}</h1>
+                <button onClick={()=>{
+                    if (light === "OFF") {
+                        setLight("ON")
+                    } else {
+                        setLight("OFF")
+                    }
+                }}>{light === "ON" ? "끄기" : "켜기"}</button>
+            </div>
+            <h1>{count}</h1>
+            <button onClick={()=>{
+                setCount(count + 1)
+            }}>+</button>
         </>
     )
 }
